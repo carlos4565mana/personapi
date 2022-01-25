042462fb8e20a7a1997dc8380c71be6b72001f7c
carlos4565mana <carloscal61@gmail.com>
2022-01-25 17:03:41 -0300

    developed person create operation

--- src/main/java/com/carlos/personapi/controller/PersonController.java
+++ src/main/java/com/carlos/personapi/controller/PersonController.java
@@ -1,24 +1,28 @@
 package com.carlos.personapi.controller;
 
+import com.carlos.personapi.dto.response.MessageResponseDTO;
 import com.carlos.personapi.entities.Person;
 import com.carlos.personapi.repositories.PersonRepository;
+import com.carlos.personapi.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
-    private PersonRepository personRepository;
+    private PersonService personService;
 
     @Autowired
-    public PersonController(PersonRepository personRepository){
-        this.personRepository = personRepository;
+    public PersonController(PersonService personService){
+        this.personService = personService;
     }
 
+
     @PostMapping
-    public String createPerson(@RequestBody Person person)
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResponseDTO createPerson(@RequestBody Person person)
     {
-        personRepository.save(person);
-        return "API test";
+        return personService.createPerson(person);
     }
 }
---
+++ src/main/java/com/carlos/personapi/services/PersonService.java
@@ -0,0 +1,29 @@
+package com.carlos.personapi.services;
+
+import com.carlos.personapi.dto.response.MessageResponseDTO;
+import com.carlos.personapi.entities.Person;
+import com.carlos.personapi.repositories.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class PersonService {
+
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository){
+        this.personRepository = personRepository;
+    }
+
+
+    public MessageResponseDTO createPerson(Person person)
+    {
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID" + savedPerson.getId())
+                .build();
+    }
+}
